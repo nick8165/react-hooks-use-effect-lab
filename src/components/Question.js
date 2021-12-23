5435dc2975ca0dc21ed4b323eba75f81b5b51d2c
nicholas shawn freeman <nick_freeman65@yahoo.com>
2021-12-22 18:48:54 -0600

    completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
@@ -9,6 +9,17 @@ function Question({ question, onAnswered }) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining((timeremaining) => timeremaining - 1)
+    }, 1000); 
+    if (timeRemaining < 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+    return () => clearTimeout(timer)
+  }, ) 
 
   const { id, prompt, answers, correctIndex } = question;
 
